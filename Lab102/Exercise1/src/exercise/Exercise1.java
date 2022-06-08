938dc6b1d14983113098e253f9af083bf7a620ec
TUANANH0 <anhtuan031999@gmail.com>
2022-06-08 14:27:10 +0700

    lab102

---
+++ Lab102/Exercise1/src/exercise/Exercise1.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 125;
+		int num2 = 2019;
+		System.out.println("Sum: " + (num1 + num2));
+	}
+
+}
